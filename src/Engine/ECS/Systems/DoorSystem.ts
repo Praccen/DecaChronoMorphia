debf8173bcaaa667319bf6b209675fd72ddbb892
Praccen <gbjorken96@hotmail.com>
2022-10-03 20:06:45 +0200

    Door now closes after you.

--- src/Engine/ECS/Systems/DoorSystem.ts
+++ src/Engine/ECS/Systems/DoorSystem.ts
@@ -16,37 +16,56 @@ export default class DoorSystem extends System {
 	update(dt: number) {
 		this.entities.forEach((e) => {
 			let doorComp = e.getComponent(ComponentTypeEnum.DOOR) as DoorComponent;
-			const collisionComp = e.getComponent(
+            
+            const collisionComp = e.getComponent(
 				ComponentTypeEnum.COLLISION
 			) as CollisionComponent;
 			let posComp = e.getComponent(
 				ComponentTypeEnum.POSITION
 			) as PositionComponent;
 
-			if (collisionComp.currentCollisionEntities.size > 0) {
-				if (!doorComp.isOpen) {
-					// Ugly maths for opening door
-					let [collidingEntity] = collisionComp.currentCollisionEntities;
-					let collidingEntityPosComp = collidingEntity.getComponent(
-						ComponentTypeEnum.POSITION
-					) as PositionComponent;
 
-					if (
-						collidingEntityPosComp &&
-						(posComp.position.x > collidingEntityPosComp.position.x ||
-							posComp.position.z > collidingEntityPosComp.position.z)
-					) {
-						posComp.position.x += 0.5;
-						posComp.position.z += 0.5;
-					} else {
-						posComp.position.x -= 0.5;
-						posComp.position.z -= 0.5;
-					}
+            if (doorComp.openDirection != 0) {
+                doorComp.doorClosingTimer += dt;
+                if (doorComp.doorClosingTimer > doorComp.doorCloseAfter) {
+                    if (doorComp.openDirection == 1) {
+                        posComp.position.x -= 0.5;
+                        posComp.position.z -= 0.5;
+                    } else {
+                        posComp.position.x += 0.5;
+                        posComp.position.z += 0.5;
+                    }
+                    posComp.rotation.y -= 90;
 
-					posComp.rotation.y += 90;
-					doorComp.isOpen = true;
-				}
-			}
+                    doorComp.doorClosingTimer = 0.0;
+                    doorComp.openDirection = 0;
+                }
+            }
+            else {
+                if (collisionComp.currentCollisionEntities.size > 0) {
+                    // Ugly maths for opening door
+                    let [collidingEntity] = collisionComp.currentCollisionEntities;
+                    let collidingEntityPosComp = collidingEntity.getComponent(
+                        ComponentTypeEnum.POSITION
+                    ) as PositionComponent;
+
+                    if (
+                        collidingEntityPosComp &&
+                        (posComp.position.x > collidingEntityPosComp.position.x ||
+                            posComp.position.z > collidingEntityPosComp.position.z)
+                    ) {
+                        posComp.position.x += 0.5;
+                        posComp.position.z += 0.5;
+                        doorComp.openDirection = 1;
+                    } else {
+                        posComp.position.x -= 0.5;
+                        posComp.position.z -= 0.5;
+                        doorComp.openDirection = -1;
+                    }
+
+                    posComp.rotation.y += 90;
+                }
+            }
 		});
 	}
 }
