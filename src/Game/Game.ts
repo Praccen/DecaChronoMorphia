f2718b4a3eb4919bb11e18849e7b9508b1681249
Emil Stolpe <emil.stolpe@swace.se>
2022-10-03 17:31:06 +0200

    moved loading of map back to game

--- src/Game/Game.ts
+++ src/Game/Game.ts
@@ -1,16 +1,24 @@
 import Rendering from "../Engine/Rendering.js";
 import ECSManager from "../Engine/ECS/ECSManager.js";
 import Player from "./Player.js";
+import { MapGenerator } from "./Map/MapGenerator.js";
+import AudioPlayer from "../Engine/Audio/AudioPlayer.js";
 
 export default class Game {
 	private rendering: Rendering;
 	private ecsManager: ECSManager;
+	private audio: AudioPlayer;
 
 	private playerObject: Player;
 
-	constructor(rendering: Rendering, ecsManager: ECSManager) {
+	constructor(
+		rendering: Rendering,
+		ecsManager: ECSManager,
+		audio: AudioPlayer
+	) {
 		this.rendering = rendering;
 		this.ecsManager = ecsManager;
+		this.audio = audio;
 
 		this.rendering.camera.setPosition(0.0, 0.0, 5.5);
 
@@ -24,6 +32,17 @@ export default class Game {
 		this.rendering.getDirectionalLight().ambientMultiplier = 0.0;
 		this.rendering.getDirectionalLight().colour.setValues(0.05, 0.05, 0.05);
 		// ----------------
+
+		// ---- Map ----
+		const mapInformation = await MapGenerator.GenerateMap(
+			5,
+			5,
+			this.ecsManager,
+			this.rendering
+		);
+		this.ecsManager.initializeSystems(mapInformation, this.audio);
+		console.log("mapInformation :>> ", mapInformation);
+		// -------------
 
 		this.playerObject.init();
 	}
