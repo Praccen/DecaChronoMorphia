057973b615f0dcac07c15c00ae3b8847f5ebfe0d
Praccen <gbjorken96@hotmail.com>
2022-10-03 19:57:03 +0200

    Random wall texture. Doors opening.

--- src/Engine/ECS/Components/DoorComponent.ts
+++ src/Engine/ECS/Components/DoorComponent.ts
@@ -1,14 +1,10 @@
 import { Component, ComponentTypeEnum } from "./Component.js";
 
 export default class DoorComponent extends Component {
-	openDirection: number;
-    doorClosingTimer: number;
-    doorCloseAfter: number;
+	isOpen: boolean;
 
 	constructor() {
 		super(ComponentTypeEnum.DOOR);
-		this.openDirection = 0;
-        this.doorClosingTimer = 0.0;
-        this.doorCloseAfter = 1.0;
+		this.isOpen = false;
 	}
 }
